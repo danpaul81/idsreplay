12a6e9c1af5e3b3bb4dacfc685780fe64d7a7350
danpaul81 <dp@danielpaul.de>
2021-05-25 17:39:31 +0200

    added container build

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 go build -o bin/idsreplay .
+docker build . -t idsreplay
+docker tag idsreplay danpaul81/idsreplay
+docker tag idsreplay harbor-repo.vmware.com/dpaul/idsreplay
+
+docker push harbor-repo.vmware.com/dpaul/idsreplay
+docker push danpaul81/idsreplay
